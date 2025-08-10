41d04c87441db6bca1ade9db71a67cd41e26184f
Mihai Chilom <mihai.chilom@yhaoo.com>
2025-08-10 22:03:46 +0300

    create property updated page

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "HomeAway Draft",
+  title: "Airbnb",
   description: "Feel at home, away from home.",
 };
 
--- utils/actions.ts
+++ utils/actions.ts
@@ -4,7 +4,12 @@ import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import db from "./db";
-import { imageSchema, profileSchema, validateWithZodSchema } from "./schemas";
+import {
+  imageSchema,
+  profileSchema,
+  propertySchema,
+  validateWithZodSchema,
+} from "./schemas";
 import { uploadImage } from "./supabase";
 
 const getAuthUser = async () => {
@@ -130,4 +135,18 @@ export const updateProfileImageAction = async (
   } catch (error) {
     return renderError(error);
   }
+};
+
+export const createPropertyAction = async (
+  prevState: unknown,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect("/");
 };
